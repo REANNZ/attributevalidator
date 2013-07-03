7b5506178eac5cfb11567fee47d242d7da9e8a2d
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2013-07-03 16:13:34 +1000

    Handle special case where auEduPersonSharedToken is invalid
    If the value is too long, we're going to hit problems trying to save
    it in the database, so we need to just error out and show the problem
    gracefully.
    
    If the value is otherwise invalid, it will end up being changed anyway
    which might cause collisions, so it's not a great idea to snapshot it.

---
+++ grails-app/views/snapshot/badSharedToken.gsp
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+  <head>
+    <meta name="layout" content="internal"/>
+  </head>
+  <body>
+
+    <div class="row">
+      <div class="span12">
+        <div class="hero-unit">
+          <h1>Invalid attribute detected</h1>
+          <p>Sorry, we weren't able to verify your released attributes.</p>
+
+          <p>An important attribute, <code>auEduPersonSharedToken</code> was not valid, which prevented us from recording a snapshot of your attributes.</p>
+
+          <p>Please report this to your helpdesk as soon as possible.</p>
+
+          <p><g:message code="branding.application.supportdesk"/></p>
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
---
+++ test/unit/aaf/av/SnapshotControllerSpec.groovy
@@ -0,0 +1,64 @@
+package aaf.av
+
+import spock.lang.*
+import org.springframework.web.servlet.ModelAndView
+
+@TestFor(SnapshotController)
+@Mock(Snapshot)
+class SnapshotControllerSpec extends spock.lang.Specification {
+  def setup() {
+    grailsApplication.config.aaf.base.realms.federated << [
+      active: true,
+      development: [ active: false ],
+    ]
+
+    def subject = Mock(org.apache.shiro.subject.Subject)
+    controller.metaClass.getSubject = {-> subject}
+
+    request.setAttribute('persistent-id', 'https://idp.one.edu.au/idp/shibboleth!-!123412341234-12341234-12341234-2134')
+    request.setAttribute('Shib-Session-ID', 'fake-sessionid-test')
+    request.setAttribute('Shib-Identity-Provider', 'https://idp.one.edu.au/idp/shibboleth')
+  }
+
+  def 'create a snapshot when a user presents only a shared token'() {
+    setup:
+    def validToken = '123456789_123456789_1234567'
+    request.setAttribute('auEduPersonSharedToken', validToken)
+
+    def subject = Mock(org.apache.shiro.subject.Subject)
+    controller.metaClass.getSubject = {-> subject}
+
+    when:
+    def model = controller.index()
+
+    then:
+    model instanceof Map
+    model.snapshot.auEduPersonSharedToken == validToken
+  }
+
+  def 'show a different view when the shared token is too short'() {
+    setup:
+    def validToken = '123456789'
+    request.setAttribute('auEduPersonSharedToken', validToken)
+
+    when:
+    def model = controller.index()
+
+    then:
+    model instanceof ModelAndView
+    model.viewName == '/snapshot/badSharedToken'
+  }
+
+  def 'show a different view when the shared token is too long'() {
+    setup:
+    def validToken = '123456789_123456789_123456789_123456789_123456789_123456789_123456789'
+    request.setAttribute('auEduPersonSharedToken', validToken)
+
+    when:
+    def model = controller.index()
+
+    then:
+    model instanceof ModelAndView
+    model.viewName == '/snapshot/badSharedToken'
+  }
+}
